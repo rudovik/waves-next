ace87fce9e318963f10ff8c887eae0e177c9d33e
rudovik <flashwrite@ukr.net>
2024-03-12 21:09:13 +0200

    Changes

--- lib/apolloWrapper.tsx
+++ lib/apolloWrapper.tsx
@@ -11,10 +11,11 @@ import {
 import { NEXT_URL } from "./NEXT_URL"
 
 // have a function to create a client for you
+
 const makeClient = () => {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "https://waves-next.vercel.app/api/graphql",
+    uri: NEXT_URL + "/api/graphql",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
