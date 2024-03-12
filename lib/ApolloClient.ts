883ff40c17fcd94f0c30f6ac5b25fab46a595970
rudovik <flashwrite@ukr.net>
2024-03-12 19:02:17 +0200

    Changes

--- lib/ApolloClient.ts
+++ lib/ApolloClient.ts
@@ -4,6 +4,7 @@ import {
   NextSSRInMemoryCache,
   NextSSRApolloClient,
 } from "@apollo/experimental-nextjs-app-support/ssr"
+import { NEXT_URL } from "./NEXT_URL"
 
 export const { getClient } = registerApolloClient(() => {
   return new NextSSRApolloClient({
@@ -11,7 +12,7 @@ export const { getClient } = registerApolloClient(() => {
     cache: new NextSSRInMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: "http://localhost:3000/api/graphql",
+      uri: NEXT_URL + "/api/graphql",
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },
