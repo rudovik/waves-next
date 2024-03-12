25a6bd31d28004a9e0b23124ba4cb48178e36eed
rudovik <flashwrite@ukr.net>
2024-03-12 20:35:58 +0200

    Changes

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -7,7 +7,7 @@ export const { getClient } = registerApolloClient(() => {
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: NEXT_URL + "/api/graphql",
+      uri: "http://localhost/api/graphql",
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },
--- lib/apolloWrapper.tsx
+++ lib/apolloWrapper.tsx
@@ -14,7 +14,7 @@ import { NEXT_URL } from "./NEXT_URL"
 const makeClient = () => {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: NEXT_URL + "/api/graphql",
+    uri: "https://waves-next.vercel.app/api/graphql",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
